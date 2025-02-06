590896d3e3b09422df6e6e52607a64c3c53f1693
Patiphan Phengpao <misterpatipan123@gmail.com>
2025-02-06 17:39:05 +0700

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import Navbar from './components/Navbar'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Header />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <h1>Header Component</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias quidem, culpa error doloribus nihil iusto dolore, modi architecto dicta ducimus exercitationem neque enim rem autem hic accusantium unde quam saepe!</p>
+    </header>
+  )
+}
+
+export default Header
