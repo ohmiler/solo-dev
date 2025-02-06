542298b027942b14d3834e2fa54f87c4e8948b09
Patiphan Phengpao <misterpatipan123@gmail.com>
2025-02-06 17:33:55 +0700

    update some code
    update app.css, app.jsx, navbar.jsx, index.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Navbar from './components/Navbar'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Navbar />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
